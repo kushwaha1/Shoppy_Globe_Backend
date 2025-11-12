25600efbde16887e696411db26335c296e8a36f4
Sweety <rsweet375@gmail.com>
2025-11-13 01:55:29 +0530

    create model for user, product and cartItem

---
+++ src/models/cartItem.model.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+const cartItemSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
+  quantity: { type: Number, default: 1, min: 1 },
+  addedAt: { type: Date, default: Date.now() }
+});
+
+cartItemSchema.index({ user: 1, product: 1 }, { unique: true });
+
+export default mongoose.model('CartItem', cartItemSchema);
